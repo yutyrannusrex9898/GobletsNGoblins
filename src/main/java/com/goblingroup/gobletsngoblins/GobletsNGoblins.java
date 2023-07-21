f210b48085c08ac6d7c4b62842632d52cd287968
Samuel <cohenzvisamuel@gmail.com>
2023-07-21 21:58:37 +0300

    First commit PS ethan was not here, that guy sucks!!!!!!!!!!

---
+++ src/main/java/com/goblingroup/gobletsngoblins/GobletsNGoblins.java
@@ -0,0 +1,43 @@
+package com.goblingroup.gobletsngoblins;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(GobletsNGoblins.MODID)
+public class GobletsNGoblins
+{
+    public static final String MODID = "gobletsngoblins";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public GobletsNGoblins()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
